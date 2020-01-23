2fcb2b18b92c93f079ad87ed5d1fa5c8891df750
Tuur <tuur.clijsters@gmail.com>
2020-01-23 15:54:41 +0100

    tweede commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,115 +5,39 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
+        <link href="/css/normalize.css" rel="stylesheet" />
+        <link href="/css/style.css" rel="stylesheet" />
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <!-- FONTS -->
+        <link href="https://fonts.googleapis.com/css?family=Bangers&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Titillium+Web&display=swap" rel="stylesheet">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-            .box{
-                background-color:red;
-                border:1px solid black;
-            }
-            .grid-container{
-                display:grid;
-                /* grid-template-columns:1fr 1fr 1fr;
-                grid-template-rows:1fr 1fr 1fr; */
-                grid-template-columns:1fr 1fr 1fr;
-                grid-template-rows:1fr 1fr 1fr;
-                grid-template-areas:
-                ". . ."
-                ". . ."
-                ". last last";
-            }
-
-            .last{
-                grid-area:last;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links grid-container">
-                    <a href="https://laravel.com/docs" class ="box">Docs</a>
-                    <a href="https://laracasts.com" class ="box">Laracasts</a>
-                    <a href="https://laravel-news.com" class ="box">News</a>
-                    <a href="https://blog.laravel.com" class ="box">Blog</a>
-                    <a href="https://nova.laravel.com" class ="box">Nova</a>
-                    <a href="https://forge.laravel.com" class ="box">Forge</a>
-                    <a href="https://vapor.laravel.com" class ="box">Vapor</a>
-                    <a href="https://github.com/laravel/laravel" class ="box last">GitHub</a>
-                </div>
+        <header>
+            <div class = "logo">
+                <img src="/img/logo4.png" alt="LOGO" height="100px" width="auto">
             </div>
-        </div>
+            <nav>
+                <ul class="navigatie">
+                    <li class ="navItem">Home</li>
+                    <li class ="navItem">About</li>
+                    <li class ="navItem">Contact</li>
+                </ul>
+            </nav>
+        </header>
+        
+        <main>
+            <div class="play">
+                PLAY
+            </div>
+        </main>
+
+        <footer>
+            <div class="copy">Copyright © 2020 by Tuur Clijsters</div>
+        </footer>
+
     </body>
 </html>
